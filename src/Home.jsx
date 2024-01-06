c3bd840b65487b8bb54896558166e5b795def82e
Ujjwal Lamichhane <47378416+Uzzal644@users.noreply.github.com>
2024-01-06 21:54:15 +0545

    learned useState hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
 import Button from "./Button"
 
 const Home = () => {
+  const[count, setCount] = useState(0)
   return (
     <div>
-        <h1>My name is Uzzal</h1>
-        <h2>It am studying web dev.</h2>
-        <Button textInsideButton = "Home" />
+      <h1>{count}</h1>
+      <button onClick={()=> setCount(count+1)}>+</button>
     </div>
+
+    // <div>
+    //     <h1>My name is Uzzal</h1>
+    //     <h2>It am studying web dev.</h2>
+    //     <Button textInsideButton = "Home" />
+    // </div>
   )
 }
 
